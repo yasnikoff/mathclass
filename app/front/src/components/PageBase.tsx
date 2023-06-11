79236ca7b54122cdba6cb34f8ca9989bd91a2139
yasnikoff <yasnikoff@gmail.com>
2023-06-12 02:41:12 +0300

    Adapt PageBase for public pages
    Sign Up page must not require auth.
    To use PageBase for sign up page,
    option to switch-off auth was added

--- app/front/src/components/PageBase.tsx
+++ app/front/src/components/PageBase.tsx
@@ -3,21 +3,15 @@ import { Error } from "../features/errors/Error"
 import { Loading } from "./Loading"
 import { LoginScreen } from "../features/auth/Login"
 import { useAuth } from "../features/auth/authHooks"
-import api from "../app/api"
-import { Button } from "react-bootstrap"
+
 export type PageBaseProps = {
-  children: ReactNode
+  children?: ReactNode
   requrieAuth: boolean
-  isLoading: boolean
+  isLoading?: boolean
   error?: unknown
 }
 export function PageBase(props: PageBaseProps) {
-  async function getError() {
-    await api.get("nosuchpage")
-  }
-
-  console.dir(props)
-  const { user } = useAuth()
+  const { user } = useAuth(props.requrieAuth)
 
   if (props.isLoading) {
     return <Loading></Loading>
@@ -28,9 +22,6 @@ export function PageBase(props: PageBaseProps) {
   return (
     <>
       <Error></Error>
-      <Button variant="danger" onClick={getError} className="m-4">
-        Simulate error
-      </Button>
       {props.children}
     </>
   )
