e7f3340061a90ef05c3c4a75668d199ef82e1fa3
yasnikoff <yasnikoff@gmail.com>
2023-06-12 02:33:21 +0300

    fix: error banner not disappearing after navigation

--- app/front/src/features/errors/Error.tsx
+++ app/front/src/features/errors/Error.tsx
@@ -2,6 +2,8 @@ import { Alert } from "react-bootstrap"
 import { useAppSelector, useAppDispatch } from "../../app/hooks"
 import { getErrorMessage } from "./errorsSelectors"
 import { setErrorMessage } from "./errorsActions"
+import { useLocation } from "react-router-dom"
+import { useEffect } from "react"
 
 export function Error() {
   const dispatch = useAppDispatch()
@@ -10,7 +12,12 @@ export function Error() {
     dispatch(setErrorMessage(""))
   }
 
+  const location = useLocation()
   const message = useAppSelector(getErrorMessage)
+  useEffect(() => {
+    dispatch(setErrorMessage(""))
+  }, [dispatch, location.pathname])
+
   if (message) {
     return (
       <Alert variant="danger" onClose={dismiss} dismissible>
