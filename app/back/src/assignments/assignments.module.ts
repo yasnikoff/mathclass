00e951c76ecf07b4b4a841e00eaaf2fa061ae256
yasnikoff <yasnikoff@gmail.com>
2023-06-14 09:24:18 +0300

    Add roles to assignments controller

--- app/back/src/assignments/assignments.module.ts
+++ app/back/src/assignments/assignments.module.ts
@@ -4,10 +4,12 @@ import { AssignmentsController } from './assignments.controller';
 import { Assignment, AssignmentSchema } from 'src/db/schemas/Assignment.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MathTestsModule } from 'src/mathTests/mathtests.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     MathTestsModule,
+    AuthModule,
     MongooseModule.forFeature([
       { name: Assignment.name, schema: AssignmentSchema },
     ]),
