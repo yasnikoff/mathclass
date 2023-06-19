e5cfac7c6dd3e134fc18d7b706f98a60ff5f40cf
yasnikoff <yasnikoff@gmail.com>
2023-06-19 14:49:01 +0300

    clean up Assignments.tsx

--- app/front/src/features/assignments/Assignments.tsx
+++ app/front/src/features/assignments/Assignments.tsx
@@ -2,32 +2,17 @@ import { PageBase } from "../../components/PageBase"
 import { Accordion, Container } from "react-bootstrap"
 import Loading from "../../components/Loading"
 import { SolutionBox } from "./SolutionBox"
-import {
-  useGetAllAssignmentsQuery,
-  useSaveAssignmentMutation,
-} from "../../app/api2"
+import { useGetAllAssignmentsQuery } from "../../app/api2"
 import { useAuth } from "../auth/authHooks"
-import { Assignment, AssignmentItem } from "."
 
 export function Assignments() {
   const { user } = useAuth()
   const isStudent = user?.role === "Student"
-  // const isTeacher = user?.role === "Teacher"
   const param = isStudent ? { studentId: user.id } : {}
 
-  const { data, error, isLoading } = useGetAllAssignmentsQuery(param)
+  const { data, isLoading } = useGetAllAssignmentsQuery(param)
 
-  const [triggerSave, saveResult] = useSaveAssignmentMutation()
-  async function save(
-    assignment: Assignment,
-    item: AssignmentItem,
-    solution: string,
-  ) {
-    item.solution = solution
-    await triggerSave(assignment).unwrap()
-  }
-
-  let assignmentsList
+  let assignmentsList = <></>
   if (isLoading) assignmentsList = <Loading></Loading>
   if (data && data?.length > 0) {
     assignmentsList = (
@@ -58,10 +43,5 @@ export function Assignments() {
     )
   }
 
-  return (
-    <PageBase requireAuth={true}>
-      {/* {studentsList} */}
-      {assignmentsList}
-    </PageBase>
-  )
+  return <PageBase requireAuth={true}>{assignmentsList}</PageBase>
 }
