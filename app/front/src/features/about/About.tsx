aa7af9c9ee20932fd95bd09972df09dcfa86848d
yasnikoff <yasnikoff@gmail.com>
2023-06-20 00:59:21 +0300

    fix About text

--- app/front/src/features/about/About.tsx
+++ app/front/src/features/about/About.tsx
@@ -8,13 +8,13 @@ export function About() {
         </Card.Header>
         <Card.Body>
           <Card.Text>
-            Mathclass helps teachers manage list of math problems, create math
+            MathClass helps teachers manage list of math problems, create math
             tests and assign them to their students.
-            <p>
-              There are two kinds of users of the Mathclass: Teachers and
-              Students. To try Mathclass, please, <a href="/login">login</a> as
-              one of the demo users or <a href="/signup">create</a> a new user.
-            </p>
+          </Card.Text>
+          <Card.Text>
+            There are two kinds of users of the MathClass: Teachers and
+            Students. To try MathClass, please, <a href="/login">login</a> as
+            one of the demo users or <a href="/signup">create</a> a new user.
           </Card.Text>
         </Card.Body>
       </Card>
