6d9d19879dcb1405db4abf13601275b833a37def
yasnikoff <yasnikoff@gmail.com>
2023-06-19 14:39:50 +0300

    fix double error banner on Assignments page

--- app/front/src/features/assignments/Assignments.tsx
+++ app/front/src/features/assignments/Assignments.tsx
@@ -31,32 +31,30 @@ export function Assignments() {
   if (isLoading) assignmentsList = <Loading></Loading>
   if (data && data?.length > 0) {
     assignmentsList = (
-      <PageBase requireAuth={true}>
-        <Accordion>
-          {data.map((assignment) => (
-            <Accordion.Item key={assignment._id} eventKey={assignment._id}>
-              <Accordion.Header>
-                <span className="p-2">Assignment:</span>{" "}
-                <b>{assignment.caption}</b>
-                <span className="p-2">Student:</span>{" "}
-                <b>{assignment?.student?.username}</b>
-              </Accordion.Header>
-              <Accordion.Body>
-                {assignment?.items?.map((item, index) => (
-                  <Container className="my-2" key={index}>
-                    <SolutionBox
-                      key={index}
-                      itemIndex={index}
-                      assignment={assignment}
-                      item={item}
-                    />
-                  </Container>
-                ))}
-              </Accordion.Body>
-            </Accordion.Item>
-          ))}
-        </Accordion>
-      </PageBase>
+      <Accordion>
+        {data.map((assignment) => (
+          <Accordion.Item key={assignment._id} eventKey={assignment._id}>
+            <Accordion.Header>
+              <span className="p-2">Assignment:</span>{" "}
+              <b>{assignment.caption}</b>
+              <span className="p-2">Student:</span>{" "}
+              <b>{assignment?.student?.username}</b>
+            </Accordion.Header>
+            <Accordion.Body>
+              {assignment?.items?.map((item, index) => (
+                <Container className="my-2" key={index}>
+                  <SolutionBox
+                    key={index}
+                    itemIndex={index}
+                    assignment={assignment}
+                    item={item}
+                  />
+                </Container>
+              ))}
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
+      </Accordion>
     )
   }
 
